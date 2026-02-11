9f3880d8bcb5d3cb09a9bbadfe81aace450006fd
Salem Hernandez <salem27@Salems-MacBook-Pro.local>
2026-02-10 23:39:25 -0800

    add components into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import Login from './components/Login'
-import authToken from './components/authToken'
-import Register from './components/Register'
-import ToDoList from './components/ToDoList'
-import './App.css'
+import './App.css';
+import Login from './components/Login';
+import authToken from './components/authToken';
+import Register from './components/Register';
+import ToDoList from './components/ToDoList';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { token, setToken, logout } = authToken();
+
+  if(token){
+    return(
+      <>
+        <button onClick={logout} style={{float: 'right'}}>Logout</button>
+        <ToDoList />
+      </>
+    );
+  }
 
   return (
     <>
-      
+      <section id='root'>
+        <h1>Welcome to My To-Do List App</h1>
+        <Login setToken={setToken}/>
+        
+        <Register setToken={setToken}/>
+      </section>
     </>
   )
 }
